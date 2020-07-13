83dd4388927ab3a4a5551a3004136d991e0f958f
eholmes <eholmes-dev@gmail.com>
2020-07-13 16:29:24 -0500

    Updating Skills & About pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,45 @@ const AboutPage = () => (
     <SEO title="About" />
     <h1>Under Construction</h1>
     <p>Coming Soon...</p>
-    
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div class="about-section">
+        <p>
+        
+          This is the about section.This is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+
+          This is the about section.This is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          This is the about sectionThis is the about sectionThis is the about sectionThis is the about section
+          </p>
+    </div>
+    <div style={{  paddingTop: '20px', maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+    </div>
+    <div class="about-section2">
+    <div class="row">
+      <div class="column">
+        <h4>Education</h4>
+          <ul>
+            <li>Software Development A.A.S. - Kirkwood Community College</li>
+          </ul>
+      </div>
+      <div class="column">
+        <h4>Certificates</h4>
+          <ul>
+            <li>Java Programming Certificate</li>
+          </ul>
+          <ul>
+            <li>.NET Programming Certificate</li>
+          </ul>
+      </div>
+    </div>      
     </div>
   </Layout>
 )
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -9,11 +9,45 @@ import SEO from "../components/seo"
 const SkillsPage = () => (
   <Layout>
     <SEO title="Skills" />
-    <h1>Under Construction</h1>
-    <p>Coming Soon...</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1 style={{ textAlign: 'left', }}>Things I'm Good At...</h1>
+    <br/>
+    <br/>
+    <br/>
+    <div class="column" style={{ position: 'relative', width: '50%'}}>
+    <div class="skills-section" style={{ textAlign: 'left', marginLeft: '100px', marginRight: '0'}}>
+      <ul>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Java</li>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>.NET C#</li>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Database & SQL</li>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Web Development</li>
+      </ul>
     </div>
+    </div>
+
+    <div class="column" style={{ position: 'relative', width: '50%'}} >
+    <div class="skills-section" style={{ textAlign: 'left', marginLeft: '150px' }}>
+      <ul>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Javascript</li>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Excel</li>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Data Analysis</li>
+        <li style={{ fontWeight: 'bold', fontSize: '25px'}}>Research</li>
+      </ul>
+    </div>
+    </div>
+    
+    <span class="skill-desc">
+      <div class="skills-java">
+        
+        <h2 style={{ marginTop: '450px'}}>Java Experience</h2>
+        <p>this is my java skill description, this is my java skill description, this is my java skill description
+        this is my java skill description, this is my java skill description, this is my java skill description
+        this is my java skill description, this is my java skill description, this is my java skill description
+        ,this is my java skill description, this is my java skill description, this is my java skill description, this is my java skill description
+        this is my java skill description, this is my java skill description
+        </p>
+      </div>
+    </span>
+
   </Layout>
 )
 
